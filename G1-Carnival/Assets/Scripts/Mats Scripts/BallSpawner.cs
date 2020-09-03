ed82bbce53d2b006cc2654e666f7a52a02bb39c7
svanmat <61579163+svanmat@users.noreply.github.com>
2020-09-03 15:11:24 +0200

    Ball Spawner & Lever Fix

---
+++ G1-Carnival/Assets/Scripts/Mats Scripts/BallSpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour
+{
+    public GameObject ballPrefab;
+    public bool leverActive { get; set; } = false;
+
+
+    private void Update()
+    {
+        if (leverActive)
+        {
+            Instantiate(ballPrefab, transform);
+        }
+    }
+}
