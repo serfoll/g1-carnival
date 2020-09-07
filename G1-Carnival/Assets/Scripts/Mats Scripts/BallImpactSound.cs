e71b25ccae4c956554f84b38bc1b906c19d498c2
svanmat <61579163+svanmat@users.noreply.github.com>
2020-09-07 10:40:37 +0200

    Ball sound and bouncyness added.

---
+++ G1-Carnival/Assets/Scripts/Mats Scripts/BallImpactSound.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallImpactSound : MonoBehaviour
+{
+    public AudioClip ballBounce;
+    AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(ballBounce);
+        }
+    }
+}
