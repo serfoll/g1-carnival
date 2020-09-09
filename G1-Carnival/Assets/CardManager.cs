d0591c5ed1a3a5fa986da748c179faba04def613
serfoll <62280198+serfoll@users.noreply.github.com>
2020-09-09 11:23:42 +0200

    Memory game in progress

--- G1-Carnival/Assets/CardManager.cs
+++ G1-Carnival/Assets/CardManager.cs
@@ -5,86 +5,109 @@ using UnityEngine;
 public class CardManager : MonoBehaviour
 {
 
-    public bool hovered { get; set; } = false;
     public bool flipped { get; set; } = false; 
 
     private GameObject currentCard;
+    private GameObject previousCard;
 
-    Vector3 defauldPosition = new Vector3 ();
-    Vector3 defauldScale = new Vector3 ();
 
-    private Animator animator;
+    private Animator currentCardAnimator;
+    private Animator previousCardAnimator;
+    
     private int cardFlipped = 0;
+    private int cardsCheck = 0;
 
-    float smooth = 5.0f;
-    float tiltAngle = 60.0f;
-
-    // Start is called before the first frame update
-    void Start ()
-    {
-        animator = gameObject.GetComponent<Animator> ();
-    }
-
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate ()
     {
 
         if ( flipped )
         {
-            if ( cardFlipped < 1 )
+            FlipCardFaceUp ();
+        }
+
+        if ( currentCard != null && previousCard != null && cardFlipped == 2)
+        {
+            CheckMatchingCard ();
+        }
+        else
+        {
+            Debug.Log ("FaceDown");
+            StopCoroutine (CardsFaceDown ());
+        }
+    }
+
+
+    //Get the current card(card picked by the player ) 
+    //if there's already a card the set it to the previous card. 
+    public void  GetCurrenCard (string _currentCardName)
+    {
+
+        if ( _currentCardName != null && currentCard == null)
+        {
+            currentCard = GameObject.Find(_currentCardName);
+            currentCardAnimator = currentCard.GetComponent<Animator>();
+            //Debug.Log (currentCard.name);
+        }
+
+        else if ( _currentCardName != null && _currentCardName != currentCard.name && currentCard != null )
+        {
+            previousCard = currentCard;
+            previousCardAnimator = previousCard.GetComponent<Animator> ();
+            currentCard = GameObject.Find (_currentCardName);
+            currentCardAnimator = currentCard.GetComponent<Animator> ();
+            //Debug.LogWarning ("PrevCard: " + previousCard.name +
+            //    "\n CurrCardd: " + currentCard.name);
+        }
+
+    }
+
+    
+    //If the player has picked two cards check to see if the cards match by using their tags
+    void CheckMatchingCard ()
+    {
+        //If the cards don't match turn them face down
+        if ( currentCard.tag != previousCard.tag)
+        {
+            Debug.LogWarning ("Cards don't match");
+            StartCoroutine (CardsFaceDown ());
+        }
+    }
+
+    void FlipCardFaceUp ()
+    {
+        if ( currentCardAnimator != null && !currentCardAnimator.GetBool ("cardFaceUp") )
+        {
+            if ( cardFlipped < 2 )
             {
-                StartCoroutine (FlipCard ());
+                currentCardAnimator.SetBool ("cardFaceUp" , true);
                 cardFlipped++;
-                Debug.Log(cardFlipped);
             }
+            Debug.Log ("Cards flipped: " + cardFlipped);
         }
-
-        if ( !hovered && currentCard != null)
-        {
-            currentCard.transform.localScale = defauldScale;
-            currentCard.transform.position = defauldPosition;
-        }
-
-
+        
     }
 
-    public void  CardAnimationController (string _currentCardName)
+   
+    IEnumerator CardsFaceDown ()
     {
-
-        if ( _currentCardName != null )
-        {
-            currentCard = GameObject.FindGameObjectWithTag (_currentCardName);
-            defauldPosition = currentCard.transform.position;
-            defauldScale = currentCard.transform.localScale;
-        }
-
-        if ( hovered )
-        {
-            currentCard.transform.localScale = new Vector3 (1.5f , 1.5f , 1.5f);
-        }
-
-    }
-
-    IEnumerator FlipCard ()
-    {
-        if ( currentCard != null)
-        {
-            float tiltAroundZ = Input.GetAxis ("Horizontal") * tiltAngle;
-            float tiltAroundX = Input.GetAxis ("Vertical") * tiltAngle;
-            //animator.SetBool ("cardFaceUp" , true);
-            //currentCard.transform.Rotate (180 , currentCard.transform.rotation.y , currentCard.transform.rotation.z);
-            Quaternion target = Quaternion.Euler (tiltAroundX , 0 , tiltAroundZ);
-            currentCard.transform.rotation = Quaternion.Slerp (currentCard.transform.rotation , target , Time.deltaTime * smooth);
-        }
         yield return new WaitForSeconds (2f);
-        //animator.SetBool ("cardFaceUp" , false);
-        //animator.SetBool ("cardFaceDown" , true);
-        yield return new WaitForSeconds (2f);
-        //animator.SetBool ("cardFaceUp" , false);
-        //animator.SetBool ("cardFaceDown" , false);
+
+        currentCardAnimator.SetBool ("cardFaceUp" , false);
+        previousCardAnimator.SetBool ("cardFaceUp" , false);
+        currentCardAnimator.SetBool ("cardFaceDown" , true);
+        previousCardAnimator.SetBool ("cardFaceDown" , true);
+        yield return new WaitForSeconds (1f);
+
         cardFlipped = 0;
-        flipped = false;
-        Debug.Log(cardFlipped);
+        currentCardAnimator.SetBool ("cardFaceDown" , false);
+        previousCardAnimator.SetBool ("cardFaceDown" , false);
+
+        currentCard = null;
+        previousCard = null;
+
+        Debug.Log ("Cards Flipped: " + currentCard);
+        Debug.Log ("Cards check: " + previousCard);
     }
+
 
 }
---
+++ G1-Carnival/Assets/Scripts/Memory/CardController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardController : MonoBehaviour
+{
+    Vector3 defauldScale = new Vector3 ();
+
+    public bool hovered { get; set; } = false;
+
+    private void Start ()
+    {
+        defauldScale = transform.localScale;
+    }
+
+    private void Update ()
+    {
+        if ( hovered )
+        {
+            transform.localScale = new Vector3 (1.5f , 1.5f , 1.5f);
+        }
+        else
+        {
+            transform.localScale = defauldScale;
+        }
+    }
+
+}
