a3c2b9c6fab5a2e40f532fbc46f03b3dfa5b16c2
svanmat <61579163+svanmat@users.noreply.github.com>
2020-09-10 11:29:37 +0200

    Hand Control

---
+++ G1-Carnival/Assets/Scripts/Mats Scripts/QuitHandControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitHandControl : MonoBehaviour
+{
+    // Quits the game
+    public void QuitTheGame()
+    {
+        Debug.Log("quitting");
+        Application.Quit();
+    }
+}
