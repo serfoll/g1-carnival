e775bdf6b049853b08fcfa604d60cd2f3e6b16b6
serfoll <62280198+serfoll@users.noreply.github.com>
2020-09-10 16:41:53 +0200

    Final Changes v2

--- G1-Carnival/Assets/Scripts/Menu/ButtonManager.cs
+++ G1-Carnival/Assets/Scripts/Menu/ButtonManager.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
@@ -8,7 +6,7 @@ public class ButtonManager : MonoBehaviour
     //Loads new scene when button on UI is pressed.
     public void ButtonMoveScene()
     {
-        Debug.Log("Hejhopp");
+        //Debug.Log("Hejhopp");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
